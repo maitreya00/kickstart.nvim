095e80acbf683aacc4c016f2dba2edda228f7437
maitreya <maitreya@xoxo.wtf>
2024-01-24 01:55:53 +0530

    add custom plugins, keymaps & options

---
+++ lua/custom/autocmds.lua
@@ -0,0 +1,9 @@
+-- Remember folds on exit
+vim.cmd([[
+  augroup remember_folds
+    autocmd!
+    autocmd BufWinLeave ?* mkview 1
+    autocmd BufWinEnter ?* silent! loadview 1
+  augroup END
+]])
+
---
+++ lua/custom/options.lua
@@ -0,0 +1,23 @@
+vim.opt.hlsearch = true
+vim.opt.scrolloff = 5
+vim.opt.wrap = true
+vim.opt.incsearch = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.cmdheight = 0
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+vim.opt.winbar = "      %f"
+vim.opt.laststatus = 3
+vim.opt.relativenumber = true
+
+-- local hr = tonumber(os.date("%H", os.time()))
+-- if hr >= 6 and hr <= 19 then -- day between 6am and 9pm
+-- 	vim.opt.background = "light"
+-- else
+-- 	vim.opt.background = "dark"
+-- end
