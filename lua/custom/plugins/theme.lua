245e5a8bbd841d41cd2fb8a77a553f401288d47c
maitreya00 <maitreya@xoxo.wtf>
2024-02-14 20:26:07 +0530

    update defaults

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,28 +1,40 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				color_overrides = {
-					mocha = {
-						base = "#000000",
-						mantle = "#000000",
-						crust = "#000000",
-					},
-				},
-				custom_highlights = function(colors)
-					return {
-						Cursor = { bg = colors.text },
-						Cursor2 = { bg = colors.text },
-						CursorLineNr = { fg = colors.mauve, style = { "bold" } },
-						CursorLine = { bg = colors.surface0 },
-					}
-				end,
-			})
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	"Mofiqul/vscode.nvim",
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        color_overrides = {
+          mocha = {
+            base = '#000000',
+            mantle = '#000000',
+            crust = '#000000',
+          },
+        },
+        custom_highlights = function(colors)
+          return {
+            Cursor = { bg = colors.text },
+            Cursor2 = { bg = colors.text },
+            CursorLineNr = { fg = colors.mauve, style = { 'bold' } },
+            CursorLine = { bg = colors.surface0 },
+          }
+        end,
+      }
+      -- vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  'Mofiqul/vscode.nvim',
+  {
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {}
+    end,
+
+    init = function()
+      -- vim.cmd 'colorscheme poimandres'
+    end,
+  },
 }
