10867c184068839753ec5cc0c7eab3175f079902
maitreya00 <maitreya@xoxo.wtf>
2024-02-14 20:34:11 +0530

    use onedark theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,5 +1,4 @@
 return {
-<<<<<<< HEAD
   {
     'catppuccin/nvim',
     name = 'catppuccin',
@@ -38,32 +37,42 @@ return {
       -- vim.cmd 'colorscheme poimandres'
     end,
   },
-=======
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				color_overrides = {
-					mocha = {
-						base = "#000000",
-						mantle = "#000000",
-						crust = "#000000",
-					},
-				},
-				custom_highlights = function(colors)
-					return {
-						Cursor = { bg = colors.text },
-						Cursor2 = { bg = colors.text },
-						CursorLineNr = { fg = colors.mauve, style = { "bold" } },
-						CursorLine = { bg = colors.surface0 },
-					}
-				end,
-			})
-			-- vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	"Mofiqul/vscode.nvim",
->>>>>>> 6fefb96 (merge upstream/master)
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        color_overrides = {
+          mocha = {
+            base = '#000000',
+            mantle = '#000000',
+            crust = '#000000',
+          },
+        },
+        custom_highlights = function(colors)
+          return {
+            Cursor = { bg = colors.text },
+            Cursor2 = { bg = colors.text },
+            CursorLineNr = { fg = colors.mauve, style = { 'bold' } },
+            CursorLine = { bg = colors.surface0 },
+          }
+        end,
+      }
+      -- vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  'Mofiqul/vscode.nvim',
+  {
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {}
+    end,
+
+    init = function()
+      -- vim.cmd 'colorscheme poimandres'
+    end,
+  },
 }
--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -1,5 +1,4 @@
 return {
-<<<<<<< HEAD
   {
     'nvim-tree/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -61,54 +60,4 @@ return {
       end)
     end,
   },
-=======
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			disable_netrw = true,
-			hijack_netrw = true,
-			update_cwd = false,
-			update_focused_file = {
-				enable = true,
-				update_cwd = false,
-			},
-			view = {
-				adaptive_size = false,
-				side = "right",
-				width = 25,
-			},
-		},
-		init = function()
-			vim.keymap.set("n", "<C-n>", "<cmd> NvimTreeToggle <CR>")
-			vim.keymap.set("i", "<C-n>", "<cmd> NvimTreeToggle <CR>")
-		end,
-	},
-	"christoomey/vim-tmux-navigator",
-	{
-		"github/copilot.vim",
-		config = function()
-			vim.g.copilot_assume_mapped = true
-			vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-		end,
-	},
-	"tpope/vim-unimpaired",
-	"tpope/vim-surround",
-	{
-		"mbbill/undotree",
-		config = function()
-			vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-		end,
-	},
-	"folke/zen-mode.nvim",
-	{
-		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	}
->>>>>>> 6fefb96 (merge upstream/master)
 }
